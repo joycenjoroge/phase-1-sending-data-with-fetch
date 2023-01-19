e43371ecf8d9d43a3de1e2f5a8c7c0ce0709e3ad
Joyce <joycenjoroge47@gmail.com>
2023-01-19 09:23:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+let name = "Steve"
+let email = "steve@steve.com"
+// let name = "Jim"
+// let email = "jim@jim.com"
+
+
+const body = document.querySelector("body");
+let message ="Unauthorized Access"
+
+
+
+function submitData(nm, emailAddress) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({
+      name:nm,
+      email:emailAddress,
+    }),
+  })
+    .then(res => res.json())
+    .then(user => document.body.innerHTML = user.id)
+    //.then(x=>console.log(x.id))
+    //.then((user) => (document.body.innerHTML = user.id))
+    .catch(function (error) {
+      // console.log(error.message)
+      document.body.innerHTML = error.message;
+    
+})
+}
+
+submitData("Steve","steve@steve.com")
+
